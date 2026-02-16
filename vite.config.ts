713d6d93f8963c3df77cc5439ed9d960cb02d7eb
Ransford Addai <addai.ransford110@gmail.com>
2026-02-16 05:01:25 +0100

    chore(ui): integrate Tailwind obfuscator and apply miscellaneous styling fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [react(),
+  obfuscator({
+    include: ['**/*.js'],
+    apply: 'build',
+    options: {
+      compact: true,
+      controlFlowFlattening: true,
+    },
+  }),
+  ],
+  build: {
+    sourcemap: false
+  }
 })
